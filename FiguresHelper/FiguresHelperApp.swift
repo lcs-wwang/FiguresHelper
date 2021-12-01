3957e50567d4a20260cd772e34e6a3fbbb2bc731
winstonwang <wwang@lcs.on.ca>
2021-12-01 09:54:15 -0500

    Initial Commit

---
+++ FiguresHelper/FiguresHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiguresHelperApp.swift
+//  FiguresHelper
+//
+//  Created by Winston Wang on 2021-12-01.
+//
+
+import SwiftUI
+
+@main
+struct FiguresHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
